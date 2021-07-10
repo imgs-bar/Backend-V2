3a279fade83bded86e600b97892ff280c59ff5f3
Pringle <80323633+PringlePot@users.noreply.github.com>
2021-07-10 20:55:07 +0200

    uploading files returns cdn url (for now)

--- src/routes/UploadRouter.ts
+++ src/routes/UploadRouter.ts
@@ -16,55 +16,67 @@ export default async function UploadRouter(router: FastifyInstance) {
     '/sharex',
     {preHandler: [uploadHandler, upload.single('file')]},
     async (request, reply) => {
-      const {user} = request;
+      try {
+        const {user} = request;
 
-      if (!user) {
-        return reply.status(403).send({
-          message: 'Unauthorized',
+        if (!user) {
+          return reply.status(403).send({
+            message: 'Unauthorized',
+          });
+        }
+        if (
+          !request.file ||
+          request.file.buffer === undefined ||
+          request.file.buffer.length === 0
+        ) {
+          return reply
+            .status(400)
+            .send({message: 'You need to provide a file!'});
+        }
+        if (
+          request.file.size! > 52_428_800 &&
+          user.roles.premium &&
+          user.roles.admin
+        ) {
+          return reply.status(413).send({message: 'File is too big!'});
+        }
+        const sha1 = crypto.createHash('sha1');
+        const hash = sha1.update(request.file.buffer).digest('hex');
+
+        const file = new File();
+
+        file.fileName = generateFileName(user, request.file.originalname);
+        file.originalFileName = request.file.originalname;
+
+        file.hash = hash;
+        file.size = request.file.size!;
+
+        file.uploader = user._id;
+
+        file.embed = {
+          ...user.settings.embeds.list[
+            Math.floor(Math.random() * user.settings.embeds.list.length)
+          ],
+          enabled: user.settings.embeds.enabled,
+        };
+
+        await file.save();
+
+        user.uploads += 1;
+        await user.save();
+
+        minio.putObject(
+          config.minio.bucket,
+          file.fileName,
+          request.file.buffer
+        );
+
+        return reply.send({
+          imageUrl: `https://${config.minio.endpoint}/${config.minio.bucket}/${file.fileName}`,
         });
+      } catch (error) {
+        return reply.status(500).send({message: error.message});
       }
-      if (
-        !request.file ||
-        request.file.buffer === undefined ||
-        request.file.buffer.length === 0
-      ) {
-        return reply.status(400).send({message: 'You need to provide a file!'});
-      }
-      if (
-        request.file.size! > 52_428_800 &&
-        user.roles.premium &&
-        user.roles.admin
-      ) {
-        return reply.status(413).send({message: 'File is too big!'});
-      }
-      const sha1 = crypto.createHash('sha1');
-      const hash = sha1.update(request.file.buffer).digest('hex');
-
-      const file = new File();
-
-      file.fileName = generateFileName(user, request.file.originalname);
-      file.originalFileName = request.file.originalname;
-
-      file.hash = hash;
-      file.size = request.file.size!;
-
-      file.uploader = user._id;
-
-      file.embed = {
-        ...user.settings.embeds.list[
-          Math.floor(Math.random() * user.settings.embeds.list.length)
-        ],
-        enabled: user.settings.embeds.enabled,
-      };
-
-      await file.save();
-
-      user.uploads += 1;
-      await user.save();
-
-      minio.putObject(config.minio.bucket, file.fileName, request.file.buffer);
-
-      return {file};
     }
   );
 }
