836d8cff1533710d79b304b458682c9a1a6f6683
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-07-30 03:09:21 +0100

    domain shit

--- src/documents/User.ts
+++ src/documents/User.ts
@@ -176,6 +176,12 @@ export interface User extends Document {
     domains: {
       //The domain's domain, i.e imgs.bar
       name: string;
+      
+      //Amount of users using domain
+      users: number
+      
+      //The domains addition date
+      dateAdded: Date;
 
       //The domains subdomain, i.e beta. Not in use with fake url
       subDomain: string;
@@ -293,6 +299,8 @@ const UserSchema: Schema = new Schema({
       type: [
         {
           name: String,
+          users: Number,
+          dateAdded: {type: Date, default: Date.now},
           subDomain: String,
           fake: Boolean,
           embeds: [String],
