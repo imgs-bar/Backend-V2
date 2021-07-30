f7fddc62bf393c50fdb02ad5376e11235fec98b5
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-07-30 02:54:21 +0100

    domain added date ( i think )

--- src/documents/Domain.ts
+++ src/documents/Domain.ts
@@ -15,6 +15,11 @@ interface Domain extends Document {
   * amount of users using the domain
   */
   users: number
+  
+  /**
+  * domain addition date
+  */
+  dateAdded: Date;
 
   /**
    * The nameservers for the domain.
@@ -51,6 +56,7 @@ const DomainSchema: Schema = new Schema({
   _id: String,
   domain: String,
   users: Number,
+  dateAdded: {type: Date, default: new Date()},
   nameservers: [String],
   setup: {type: Boolean, default: false},
   private: Boolean,
