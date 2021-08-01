b7f7f4bc9af92419c42abcb9d2b9a581d766deff
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-08-01 15:57:01 -0500

    Add: Change UID endpoint for bots

---
+++ src/routes/BotRouter.ts
@@ -0,0 +1,45 @@
+import { FastifyInstance } from "fastify";
+import { User } from "../documents/User";
+import { botHandler } from "../handlers/BotHandler";
+import { ResolveUser } from "../handlers/ResolveUser";
+
+export default function BotRouter(router: FastifyInstance, opts: any, done: any) {
+    router.post('/changeuid', {
+        preHandler: [botHandler, ResolveUser],
+        schema: {
+            body: {
+                type: 'object',
+                required: ['uid', 'user'],
+                properties: {
+                    uid: { type: 'integer' },
+                    user: { type: 'string' }
+                },
+            },
+        },
+    }, async (req, res) => {
+        try {
+            const { user } = req
+            if (typeof user !== 'object') {
+                res.status(500).send({ error: 'Well, that shouldnt have happened' });
+                return;
+            }
+
+            const NewUID = (req.body as any).uid
+    
+            const UIDTakenCheck = await User.findOne({ _id: NewUID });
+            if (UIDTakenCheck) return res.status(400).send({ message: 'UID already in use' });
+    
+            user.uid = NewUID
+            user.save()
+    
+            res.status(200).send({ message: 'Updated UID successfully' })
+            return;
+        } catch (err) {
+            res.status(500).send({ error: err.message })
+            return;
+        }
+    })
+
+    done()
+}
+export const autoPrefix = '/bot';
