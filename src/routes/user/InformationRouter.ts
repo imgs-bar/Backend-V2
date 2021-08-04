4d25a687c46fba9efc1a9974084c50a2ccff3a02
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-08-04 08:01:21 +0100

    /files - neko

--- src/routes/user/InformationRouter.ts
+++ src/routes/user/InformationRouter.ts
@@ -1,5 +1,7 @@
 import Filter from 'bad-words';
 import {FastifyInstance} from 'fastify';
+import {File} from '../../documents/File';
+import {minio} from '../../config/config.json';
 import {authHandler} from '../../handlers/AuthHandler';
 import {botHandler} from '../../handlers/BotHandler';
 import {getFromRedis, setInRedis} from '../../util/RedisUtil';
@@ -12,6 +14,30 @@ export default async function AuthRouter(router: FastifyInstance) {
   router.get('/motd', {preHandler: authHandler}, async (req, reply) => {
     const motd = await getFromRedis('motd', 'Message of the day');
     reply.send({motd});
+  });
+
+  router.get('/images', {preHandler: authHandler}, async (req, reply) => {
+    const {user} = req;
+
+    if (!user)
+      return reply.status(500).send({message: "This shouldn't happen"});
+
+    const fileInfo = [];
+
+    const files = await File.find({'uploader.id': user._id});
+
+    files.forEach(file => {
+      if (file.deleted) return;
+      fileInfo.push({
+        link: `https://beta.aint.cool/${file.fileName}`,
+        cdnLink: `https://${minio.endpoint}/imgs-beta/${file.cdnFileName}`,
+        uploadedAt: file.uploadedAt,
+        fileSize: file.size,
+        originalFileName: file.originalFileName,
+      });
+    });
+
+    return reply.send({files});
   });
 
   router.patch<{Body: setMotdInterface; Headers: botInterface}>(
